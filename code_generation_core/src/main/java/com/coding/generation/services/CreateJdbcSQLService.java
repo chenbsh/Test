766f5770156d67b578b1f3bd8042147a571a92ec
chenbingshuang <chenbingshuang@eiker.com.cn>
2022-10-08 14:15:51 +0800

    当存在多个删除属性，生成DELETE的SQL语句时关联多个属性联合删除，优化成一个删除属性一个函数

--- code_generation_core/src/main/java/com/coding/generation/services/CreateJdbcSQLService.java
+++ code_generation_core/src/main/java/com/coding/generation/services/CreateJdbcSQLService.java
@@ -29,213 +29,195 @@ import com.coding.jdbc.exception.SYHCException;
 @Repository
 public class CreateJdbcSQLService {
 
-	@Resource
-	private CreateCodingFileTools fileTools;
+    @Resource
+    private CreateCodingFileTools fileTools;
 
-	/**
-	 * 生成目标库表JDBC业务SQL语句
-	 *
-	 * @param databaseName 库实例名称
-	 *
-	 * @param tableName 数据表名称
-	 *
-	 * @param tableComment 数据表名称注解
-	 *
-	 * @param columnMap 库表列字段定义
-	 *
-	 * @param defaultMethod 默认方法定义
-	 *
-	 * @return Boolean true/false=成功/失败
-	 *
-	 * @throws SYHCException
-	 */
-	public Boolean constractSql(String databaseName, String tableName, String tableComment, Map<String, TableColumnBean> columnMap, String defaultMethod[]) throws SYHCException {
-		Boolean conditionFlag = false;
-		Boolean statisticsFlag = false;
-		// 1.生成SQL语句
-		List<String> sqlList = new ArrayList<String>();
-		if (defaultMethod != null) {
-			for (String method : defaultMethod) {
-				if (method.equals("insert")) {
-					sqlList.add("#新增" + tableComment + "信息实例");
-					sqlList.add(insertSQL(databaseName, tableName, columnMap.values()));
-				} else if (method.equals("update")) {
-					sqlList.add("#更新" + tableComment + "信息实例");
-					sqlList.add(updateSQL(databaseName, tableName, columnMap.values(), false));
-				} else if (method.equals("updateDynamicField")) {
-					sqlList.add("#动态更新" + tableComment + "列字段值");
-					sqlList.add(updateSQL(databaseName, tableName, columnMap.values(), true));
-				} else if (method.equals("condition")) {
-					conditionFlag = true;
-				} else if (method.equals("statistics")) {
-					statisticsFlag = true;
-				}
-			}
-		}
-		for (TableColumnBean column : columnMap.values()) {
-			if (StringUtils.isNotBlank(column.getRemoveMothod())) {
-				this.deleteSQL(databaseName, tableName, tableComment, columnMap.values(), sqlList);
-			}
-		}
-		String basicSQL = this.queryForBean(databaseName, tableName, columnMap.values());
-		sqlList.add("#获取" + tableComment + "信息实例【基础查询语句】");
-		sqlList.add(this.propertyHead(tableName) + "_queryForBean=" + basicSQL);
+    /**
+     * 生成目标库表JDBC业务SQL语句
+     *
+     * @param databaseName  库实例名称
+     * @param tableName     数据表名称
+     * @param tableComment  数据表名称注解
+     * @param columnMap     库表列字段定义
+     * @param defaultMethod 默认方法定义
+     * @return Boolean true/false=成功/失败
+     * @throws SYHCException
+     */
+    public Boolean constractSql(String databaseName, String tableName, String tableComment, Map<String, TableColumnBean> columnMap, String defaultMethod[]) throws SYHCException {
+        Boolean conditionFlag = false;
+        Boolean statisticsFlag = false;
+        // 1.生成SQL语句
+        List<String> sqlList = new ArrayList<String>();
+        if (defaultMethod != null) {
+            for (String method : defaultMethod) {
+                if (method.equals("insert")) {
+                    sqlList.add("#新增" + tableComment + "信息实例");
+                    sqlList.add(insertSQL(databaseName, tableName, columnMap.values()));
+                } else if (method.equals("update")) {
+                    sqlList.add("#更新" + tableComment + "信息实例");
+                    sqlList.add(updateSQL(databaseName, tableName, columnMap.values(), false));
+                } else if (method.equals("updateDynamicField")) {
+                    sqlList.add("#动态更新" + tableComment + "列字段值");
+                    sqlList.add(updateSQL(databaseName, tableName, columnMap.values(), true));
+                } else if (method.equals("condition")) {
+                    conditionFlag = true;
+                } else if (method.equals("statistics")) {
+                    statisticsFlag = true;
+                }
+            }
+        }
+        for (TableColumnBean column : columnMap.values()) {
+            if (StringUtils.isNotBlank(column.getRemoveMothod())) {
+                this.deleteSQL(databaseName, tableName, tableComment, column, sqlList);
+            }
+        }
+        String basicSQL = this.queryForBean(databaseName, tableName, columnMap.values());
+        sqlList.add("#获取" + tableComment + "信息实例【基础查询语句】");
+        sqlList.add(this.propertyHead(tableName) + "_queryForBean=" + basicSQL);
 
-		for (TableColumnBean column : columnMap.values()) {
-			if (StringUtils.isNotBlank(column.getBeanMothod())) {
-				this.queryForBean(tableName, tableComment, column, sqlList);
-			}
-		}
-		for (TableColumnBean column : columnMap.values()) {
-			if (StringUtils.isNotBlank(column.getListMothod())) {
-				this.queryForList(tableName, tableComment, column, sqlList);
-			}
-		}
-		if (conditionFlag) {
-			this.conditionSQL(tableName, tableComment, sqlList);
-		}
-		if (statisticsFlag) {
-			this.statisticsSQL(databaseName, tableName, tableComment, sqlList);
-		}
-		// 生成SQL语句
-		fileTools.writeFile(sqlList, databaseName, tableName, tableName + ".sql");
-		return true;
-	}
+        for (TableColumnBean column : columnMap.values()) {
+            if (StringUtils.isNotBlank(column.getBeanMothod())) {
+                this.queryForBean(tableName, tableComment, column, sqlList);
+            }
+        }
+        for (TableColumnBean column : columnMap.values()) {
+            if (StringUtils.isNotBlank(column.getListMothod())) {
+                this.queryForList(tableName, tableComment, column, sqlList);
+            }
+        }
+        if (conditionFlag) {
+            this.conditionSQL(tableName, tableComment, sqlList);
+        }
+        if (statisticsFlag) {
+            this.statisticsSQL(databaseName, tableName, tableComment, sqlList);
+        }
+        // 生成SQL语句
+        fileTools.writeFile(sqlList, databaseName, tableName, tableName + ".sql");
+        return true;
+    }
 
-	private String propertyHead(String tableName) {
-		return tableName.replaceFirst("tb_", "").replaceFirst("v_", "").replaceFirst("mv_", "");
-	}
+    private String propertyHead(String tableName) {
+        return tableName.replaceFirst("tb_", "").replaceFirst("v_", "").replaceFirst("mv_", "");
+    }
 
-	private String insertSQL(String databaseName, String tableName, Collection<TableColumnBean> columnList) {
-		String propertyKey = this.propertyHead(tableName) + "_insert=";
-		StringBuilder sql = new StringBuilder(propertyKey + "insert into " +  tableName + " (");
-		Boolean isFirst = true;
-		for (TableColumnBean column : columnList) {
-			if (isFirst) {
-				sql.append(column.getColumnName());
-			} else {
-				sql.append("," + column.getColumnName());
-			}
-			isFirst = false;
-		}
-		sql.append(") values (");
-		isFirst = true;
-		for (TableColumnBean column : columnList) {
-			if (isFirst == false) {
-				sql.append(",");
-			}
-			sql.append(column.getVariableName());
-			isFirst = false;
-		}
-		sql.append(")");
-		return sql.toString();
+    private String insertSQL(String databaseName, String tableName, Collection<TableColumnBean> columnList) {
+        String propertyKey = this.propertyHead(tableName) + "_insert=";
+        StringBuilder sql = new StringBuilder(propertyKey + "insert into " + tableName + " (");
+        Boolean isFirst = true;
+        for (TableColumnBean column : columnList) {
+            if (isFirst) {
+                sql.append(column.getColumnName());
+            } else {
+                sql.append("," + column.getColumnName());
+            }
+            isFirst = false;
+        }
+        sql.append(") values (");
+        isFirst = true;
+        for (TableColumnBean column : columnList) {
+            if (isFirst == false) {
+                sql.append(",");
+            }
+            sql.append(column.getVariableName());
+            isFirst = false;
+        }
+        sql.append(")");
+        return sql.toString();
 
-	}
+    }
 
-	private String updateSQL(String databaseName, String tableName, Collection<TableColumnBean> columnList, Boolean dynamic) {
-		StringBuilder sql = null;
-		Boolean isFirst = true;
-		String propertyKey = this.propertyHead(tableName);
-		if (dynamic == false) {
-			propertyKey = propertyKey + "_update=";
-			sql = new StringBuilder(propertyKey + "update " +  tableName + " set ");
-			for (TableColumnBean column : columnList) {
-				if (column.getColumnName().toUpperCase().equals("CREATETIME") || column.getColumnName().toUpperCase().equals("INSERTTIME") || column.getPrimaryKey()) {
-					continue;
-				}
-				if (isFirst == false) {
-					sql.append(",");
-				}
-				isFirst = false;
-				sql.append(column.getColumnName() + "=" + column.getVariableName());
-			}
-		} else {
-			propertyKey = propertyKey + "_updateDynamicField=";
-			sql = new StringBuilder(propertyKey + "upadte " +  tableName + " set #condition# ");
-		}
-		sql.append(" where ");
-		isFirst = true;
-		for (TableColumnBean column : columnList) {
-			if (column.getPrimaryKey()) {
-				if (isFirst == false) {
-					sql.append(" and ");
-				} else {
-					isFirst = false;
-				}
-				sql.append(column.getColumnName() + "=" + column.getVariableName());
-			}
-		}
-		return sql.toString();
-	}
+    private String updateSQL(String databaseName, String tableName, Collection<TableColumnBean> columnList, Boolean dynamic) {
+        StringBuilder sql = null;
+        Boolean isFirst = true;
+        String propertyKey = this.propertyHead(tableName);
+        if (dynamic == false) {
+            propertyKey = propertyKey + "_update=";
+            sql = new StringBuilder(propertyKey + "update " + tableName + " set ");
+            for (TableColumnBean column : columnList) {
+                if (column.getColumnName().toUpperCase().equals("CREATETIME") || column.getColumnName().toUpperCase().equals("INSERTTIME") || column.getPrimaryKey()) {
+                    continue;
+                }
+                if (isFirst == false) {
+                    sql.append(",");
+                }
+                isFirst = false;
+                sql.append(column.getColumnName() + "=" + column.getVariableName());
+            }
+        } else {
+            propertyKey = propertyKey + "_updateDynamicField=";
+            sql = new StringBuilder(propertyKey + "upadte " + tableName + " set #condition# ");
+        }
+        sql.append(" where ");
+        isFirst = true;
+        for (TableColumnBean column : columnList) {
+            if (column.getPrimaryKey()) {
+                if (isFirst == false) {
+                    sql.append(" and ");
+                } else {
+                    isFirst = false;
+                }
+                sql.append(column.getColumnName() + "=" + column.getVariableName());
+            }
+        }
+        return sql.toString();
+    }
 
-	private void deleteSQL(String databaseName, String tableName, String tableComment, Collection<TableColumnBean> columnList, List<String> sqlList) {
-		Boolean isFirst = true;
-		String columnName = null;
-		String columnComment = null;
-		StringBuilder where = new StringBuilder(" where ");
-		for (TableColumnBean column : columnList) {
-			if (column.getPrimaryKey()) {
-				if (isFirst == false) {
-					where.append(" and ");
-					columnComment = columnComment + "和" + column.getColumnComment();
-					columnName = columnName + "And" + column.getColumnName().substring(0, 1).toUpperCase() + column.getColumnName().substring(1, column.getColumnName().length());
-				} else {
-					isFirst = false;
-					columnComment = column.getColumnComment();
-					columnName = column.getColumnName().substring(0, 1).toUpperCase() + column.getColumnName().substring(1, column.getColumnName().length());
-				}
-				where.append(column.getColumnName() + "=" + column.getVariableName());
-			}
-		}
-		sqlList.add("#根据" + columnComment + "删除" + tableComment + "信息实例");
-		String propertyKey = this.propertyHead(tableName) + "_deleteBy";
-		sqlList.add(propertyKey + columnName + "=delete from " +  tableName + where);
-	}
+    private void deleteSQL(String databaseName, String tableName, String tableComment, TableColumnBean column, List<String> sqlList) {
+        String columnName = null;
+        String columnComment = null;
+        StringBuilder where = new StringBuilder(" where ");
+        columnComment = column.getColumnComment();
+        columnName = column.getColumnName().substring(0, 1).toUpperCase() + column.getColumnName().substring(1, column.getColumnName().length());
+        where.append(column.getColumnName() + "=" + column.getVariableName());
+        sqlList.add("#根据" + columnComment + "删除" + tableComment + "信息实例");
+        String propertyKey = this.propertyHead(tableName) + "_deleteBy";
+        sqlList.add(propertyKey + columnName + "=delete from " + tableName + where);
+    }
 
-	private void queryForBean(String tableName, String tableComment, TableColumnBean column, List<String> sqlList) {
-		String columnName = null;
-		columnName = column.getColumnName();
-		columnName = columnName.substring(0, 1).toUpperCase() + columnName.substring(1, columnName.length());
-		String propertyKey = this.propertyHead(tableName) + "_queryForBeanBy" + columnName;
+    private void queryForBean(String tableName, String tableComment, TableColumnBean column, List<String> sqlList) {
+        String columnName = null;
+        columnName = column.getColumnName();
+        columnName = columnName.substring(0, 1).toUpperCase() + columnName.substring(1, columnName.length());
+        String propertyKey = this.propertyHead(tableName) + "_queryForBeanBy" + columnName;
 
-		sqlList.add("#根据" + column.getColumnComment() + "查询" + tableComment + "信息实例");
-		sqlList.add(propertyKey + "=" + "where t." + column.getColumnName() + "=" + column.getVariableName());
-	}
+        sqlList.add("#根据" + column.getColumnComment() + "查询" + tableComment + "信息实例");
+        sqlList.add(propertyKey + "=" + "where t." + column.getColumnName() + "=" + column.getVariableName());
+    }
 
-	private String queryForBean(String databaseName, String tableName, Collection<TableColumnBean> columnList) {
-		Boolean isFirst = true;
-		StringBuilder sql = new StringBuilder("select ");
-		for (TableColumnBean column : columnList) {
-			if (isFirst == false) {
-				sql.append(",");
-			}
-			isFirst = false;
-			sql.append(column.getQueryField());
-		}
-		sql.append(" from " +  tableName + " t");
-		return sql.toString();
-	}
+    private String queryForBean(String databaseName, String tableName, Collection<TableColumnBean> columnList) {
+        Boolean isFirst = true;
+        StringBuilder sql = new StringBuilder("select ");
+        for (TableColumnBean column : columnList) {
+            if (isFirst == false) {
+                sql.append(",");
+            }
+            isFirst = false;
+            sql.append(column.getQueryField());
+        }
+        sql.append(" from " + tableName + " t");
+        return sql.toString();
+    }
 
-	private void queryForList(String tableName, String tableComment, TableColumnBean column, List<String> sqlList) {
-		String columnName = null;
-		columnName = column.getColumnName();
-		columnName = columnName.substring(0, 1).toUpperCase() + columnName.substring(1, columnName.length());
-		String propertyKey = this.propertyHead(tableName) + "_queryForListBy" + columnName;
+    private void queryForList(String tableName, String tableComment, TableColumnBean column, List<String> sqlList) {
+        String columnName = null;
+        columnName = column.getColumnName();
+        columnName = columnName.substring(0, 1).toUpperCase() + columnName.substring(1, columnName.length());
+        String propertyKey = this.propertyHead(tableName) + "_queryForListBy" + columnName;
 
-		sqlList.add("#根据" + column.getColumnComment() + "查询" + tableComment + "信息实例列表");
-		sqlList.add(propertyKey + "=" + "where t." + column.getColumnName() + "=" + column.getVariableName());
-	}
+        sqlList.add("#根据" + column.getColumnComment() + "查询" + tableComment + "信息实例列表");
+        sqlList.add(propertyKey + "=" + "where t." + column.getColumnName() + "=" + column.getVariableName());
+    }
 
-	private void conditionSQL(String tableName, String tableComment, List<String> sqlList) {
-		sqlList.add("#根据动态查询条件获取" + tableComment + "信息实例集合");
-		String propertyKey = this.propertyHead(tableName) + "_queryForListByCondition=";
-		sqlList.add(propertyKey + "#condition# limit #startIndex#,#pageSize#");
-	}
+    private void conditionSQL(String tableName, String tableComment, List<String> sqlList) {
+        sqlList.add("#根据动态查询条件获取" + tableComment + "信息实例集合");
+        String propertyKey = this.propertyHead(tableName) + "_queryForListByCondition=";
+        sqlList.add(propertyKey + "#condition# limit #startIndex#,#pageSize#");
+    }
 
-	private void statisticsSQL(String databaseName, String tableName, String tableComment, List<String> sqlList) {
-		sqlList.add("#根据动态查询条件统计" + tableComment + "信息实例记录数");
-		String propertyKey = this.propertyHead(tableName) + "_statisticsForListByCondition=";
-		sqlList.add(propertyKey + "select count(1) as totalno from " +  tableName + " t #condition#");
-	}
+    private void statisticsSQL(String databaseName, String tableName, String tableComment, List<String> sqlList) {
+        sqlList.add("#根据动态查询条件统计" + tableComment + "信息实例记录数");
+        String propertyKey = this.propertyHead(tableName) + "_statisticsForListByCondition=";
+        sqlList.add(propertyKey + "select count(1) as totalno from " + tableName + " t #condition#");
+    }
 
 }
--- code_generation_core/src/main/java/com/coding/generation/services/MysqlTableReflectionDefinePersistenceLogicService.java
+++ code_generation_core/src/main/java/com/coding/generation/services/MysqlTableReflectionDefinePersistenceLogicService.java
@@ -14,7 +14,7 @@ import com.coding.jdbc.tools.SpringJdbcTemplateTool;
 
 /**
  * 数据库库表生成代码配置定义信息数据库持久化类生成工具
- * 
+ *
  * @Copyright MacChen
  * @Project CodeGenerationTool
  * @Author MacChen
@@ -27,98 +27,91 @@ import com.coding.jdbc.tools.SpringJdbcTemplateTool;
 @Repository
 public class MysqlTableReflectionDefinePersistenceLogicService {
 
-	private Log logger = LogFactory.getLog(this.getClass().getName());
+    private Log logger = LogFactory.getLog(this.getClass().getName());
 
-	public MysqlTableReflectionDefinePersistenceLogicService() {
-		super();
-	}
+    public MysqlTableReflectionDefinePersistenceLogicService() {
+        super();
+    }
 
-	@Resource(name = "basicJdbcTarget")
-	private SpringJdbcTemplateTool jdbc;
+    @Resource(name = "basicJdbcTarget")
+    private SpringJdbcTemplateTool jdbc;
 
-	// 保存库表代码生成定义信息
-	@Value("${reflection_define_insert}")
-	private String reflection_define_insert;
+    // 保存库表代码生成定义信息
+    @Value("${reflection_define_insert}")
+    private String reflection_define_insert;
 
-	// 更新库表代码生成定义信息
-	@Value("${reflection_define_update}")
-	private String reflection_define_update;
+    // 更新库表代码生成定义信息
+    @Value("${reflection_define_update}")
+    private String reflection_define_update;
 
-	// 根据库表名称获取库表代码生成定义信息
-	@Value("${reflection_define_queryForBean}")
-	private String reflection_define_queryForBean;
+    // 根据库表名称获取库表代码生成定义信息
+    @Value("${reflection_define_queryForBean}")
+    private String reflection_define_queryForBean;
 
-	/**
-	 * 添加数据库库表生成代码配置定义信息实例
-	 * 
-	 * @param MysqlTableReflectionDefineBean entity 数据库库表生成代码配置定义信息实例
-	 * 
-	 * @return Boolean true:false=成功:失败
-	 * 
-	 * @throws SYHCException
-	 */
-	public Boolean insert(MysqlTableReflectionDefineBean entity) throws SYHCException {
-		String sql = null;
-		try {
-			sql = this.reflection_define_insert;
-			sql = AttributeMappingTool.constractSql(sql, entity);
-			logger.info("-------------添加数据库库表生成代码配置定义信息实例：\n" + sql);
-			return jdbc.execute(sql) == 1;
-		} catch (Exception e) {
-			e.printStackTrace();
-			String warning = "------------添加数据库库表生成代码配置定义信息实例：" + SYHCException.JDBC_EXECUTE_ONLY + "\n" + sql + "\n";
-			logger.error(warning + e.getMessage());
-			throw new SYHCException(warning);
-		}
-	}
+    /**
+     * 添加数据库库表生成代码配置定义信息实例
+     *
+     * @param entity 数据库库表生成代码配置定义信息实例
+     * @return Boolean true:false=成功:失败
+     * @throws SYHCException
+     */
+    public Boolean insert(MysqlTableReflectionDefineBean entity) throws SYHCException {
+        String sql = null;
+        try {
+            sql = this.reflection_define_insert;
+            sql = AttributeMappingTool.constractSql(sql, entity);
+            logger.info("-------------添加数据库库表生成代码配置定义信息实例：\n" + sql);
+            return jdbc.execute(sql) == 1;
+        } catch (Exception e) {
+            e.printStackTrace();
+            String warning = "------------添加数据库库表生成代码配置定义信息实例：" + SYHCException.JDBC_EXECUTE_ONLY + "\n" + sql + "\n";
+            logger.error(warning + e.getMessage());
+            throw new SYHCException(warning);
+        }
+    }
 
-	/**
-	 * 更新数据库库表生成代码配置定义信息实例
-	 * 
-	 * @param MysqlTableReflectionDefineBean entity 数据库库表生成代码配置定义信息实例
-	 * 
-	 * @return Boolean true:false=成功:失败
-	 * 
-	 * @throws SYHCException
-	 */
-	public Boolean update(MysqlTableReflectionDefineBean entity) throws SYHCException {
-		String sql = null;
-		try {
-			sql = this.reflection_define_update;
-			sql = AttributeMappingTool.constractSql(sql, entity);
-			logger.info("-------------更新数据库库表生成代码配置定义信息实例：\n" + sql);
-			return jdbc.execute(sql) == 1;
-		} catch (Exception e) {
-			e.printStackTrace();
-			String warning = "------------更新数据库库表生成代码配置定义信息实例：" + SYHCException.JDBC_EXECUTE_ONLY + "\n" + sql + "\n";
-			logger.error(warning + e.getMessage());
-			throw new SYHCException(warning);
-		}
-	}
+    /**
+     * 更新数据库库表生成代码配置定义信息实例
+     *
+     * @param entity 数据库库表生成代码配置定义信息实例
+     * @return Boolean true:false=成功:失败
+     * @throws SYHCException
+     */
+    public Boolean update(MysqlTableReflectionDefineBean entity) throws SYHCException {
+        String sql = null;
+        try {
+            sql = this.reflection_define_update;
+            sql = AttributeMappingTool.constractSql(sql, entity);
+            logger.info("-------------更新数据库库表生成代码配置定义信息实例：\n" + sql);
+            return jdbc.execute(sql) == 1;
+        } catch (Exception e) {
+            e.printStackTrace();
+            String warning = "------------更新数据库库表生成代码配置定义信息实例：" + SYHCException.JDBC_EXECUTE_ONLY + "\n" + sql + "\n";
+            logger.error(warning + e.getMessage());
+            throw new SYHCException(warning);
+        }
+    }
 
-	/**
-	 * 根据库表名称获取库表代码生成定义信息
-	 * 
-	 * @param databaseName 数据库实例名称
-	 * 
-	 * @param tableName 数据库表名称
-	 * 
-	 * @return MysqlTableReflectionDefineBean entity 数据库库表生成代码配置定义信息实例
-	 * 
-	 * @throws SYHCException
-	 */
-	public MysqlTableReflectionDefineBean queryForBean(String databaseName, String tableName) throws SYHCException {
-		String sql = null;
-		try {
-			sql = this.reflection_define_queryForBean;
-			sql = AttributeMappingTool.constractSql(sql, new String[][] { { "databaseName", databaseName }, { "tableName", tableName } });
-			logger.info("-------------根据库表名称获取库表代码生成定义信息实例：\n" + sql);
-			return jdbc.queryForBean(sql, MysqlTableReflectionDefineBean.class);
-		} catch (Exception e) {
-			e.printStackTrace();
-			String warning = "------------根据库表名称获取库表代码生成定义信息实例：" + SYHCException.JDBC_SELECT_ONLY + "\n" + sql + "\n";
-			logger.error(warning + e.getMessage());
-			throw new SYHCException(warning);
-		}
-	}
+    /**
+     * 根据库表名称获取库表代码生成定义信息
+     *
+     * @param databaseName 数据库实例名称
+     * @param tableName    数据库表名称
+     * @return MysqlTableReflectionDefineBean entity 数据库库表生成代码配置定义信息实例
+     * @throws SYHCException
+     */
+    public MysqlTableReflectionDefineBean queryForBean(String databaseName, String tableName) throws SYHCException {
+        String sql = null;
+        try {
+            sql = this.reflection_define_queryForBean;
+            sql = AttributeMappingTool.constractSql(sql, new String[][]{{"databaseName", databaseName}, {"tableName", tableName}});
+            logger.info("-------------根据库表名称获取库表代码生成定义信息实例：\n" + sql);
+            return jdbc.queryForBean(sql, MysqlTableReflectionDefineBean.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+            String warning = "------------根据库表名称获取库表代码生成定义信息实例：" + SYHCException.JDBC_SELECT_ONLY + "\n" + sql + "\n";
+            logger.error(warning + e.getMessage());
+            throw new SYHCException(warning);
+        }
+    }
 }
